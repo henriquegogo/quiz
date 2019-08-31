6e5fe2fb8296575692f5a7c247c807ba52e5d319
Henrique Gogó <henriquegogo@gmail.com>
2019-08-31 19:45:05 -0300

    Builded app as gh-pages

---
+++ precache-manifest.12b49dc704834cac259e6bd167450c9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "638ff9642da32bdfd7e70cecb9159981",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f6d7cb4977e769a9775",
+    "url": "/static/css/main.d83e1da6.chunk.css"
+  },
+  {
+    "revision": "d1df01854c02d1230925",
+    "url": "/static/js/2.3dd0a847.chunk.js"
+  },
+  {
+    "revision": "8f6d7cb4977e769a9775",
+    "url": "/static/js/main.57c4d849.chunk.js"
+  },
+  {
+    "revision": "ebd06a4b9bc4db342ea9",
+    "url": "/static/js/runtime~main.340193ae.js"
+  }
+]);
