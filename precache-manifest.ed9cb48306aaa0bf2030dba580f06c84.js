9285c9237cd6f615727d8e97dc69fe8c4c458426
Henrique Soares <henrique.vasconcelos@hp.com>
2019-09-02 18:04:16 -0300

    Deploy

---
+++ precache-manifest.ed9cb48306aaa0bf2030dba580f06c84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "920ceee03f96b87da82c56b4311a6caf",
+    "url": "https://henriquegogo.github.io/quiz/index.html"
+  },
+  {
+    "revision": "d2331d5978208670cffa",
+    "url": "https://henriquegogo.github.io/quiz/static/css/main.bdc8d630.chunk.css"
+  },
+  {
+    "revision": "837f791d0947b4599090",
+    "url": "https://henriquegogo.github.io/quiz/static/js/2.173bd393.chunk.js"
+  },
+  {
+    "revision": "d2331d5978208670cffa",
+    "url": "https://henriquegogo.github.io/quiz/static/js/main.2453ae0f.chunk.js"
+  },
+  {
+    "revision": "d6412c7e116c34a22f26",
+    "url": "https://henriquegogo.github.io/quiz/static/js/runtime~main.4f52fe40.js"
+  }
+]);
